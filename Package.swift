dd9102d8c0477afef5ad8e72662ce1d3f6191182
Navan Chauhan <navanchauhan@gmail.com>
2023-12-09 20:57:20 -0700

    add initial docs

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
     ),
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
