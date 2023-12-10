cfae29a68f8ae5e2ba5feeea24e3943e87b04a22
Navan Chauhan <navanchauhan@gmail.com>
2023-12-09 17:34:26 -0700

    Fix comments not displaying on some clients

--- Sources/Helpers.swift
+++ Sources/Helpers.swift
@@ -3,7 +3,7 @@ import Foundation
 let versionString = "generated and served by swift-gopher/1.0.0" // TODO: Handle automatic versioning
 
 func buildVersionStringResponse() -> String {
-    let repeatedString = "i" + String(repeating: "-", count: 80) + "\terror.host\t1\r\n"
-    let versionResponseString = "i" + String(repeating: " ", count: 80 - versionString.count) + versionString + "\terror.host\t1\r\n"
+    let repeatedString = "i" + String(repeating: "-", count: 80) + "\t\terror.host\t1\r\n"
+    let versionResponseString = "i" + String(repeating: " ", count: 80 - versionString.count) + versionString + "\t\terror.host\t1\r\n"
     return "\(repeatedString)\(versionResponseString)"
 }
